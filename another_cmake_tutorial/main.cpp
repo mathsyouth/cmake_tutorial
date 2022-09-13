41971c9d143a413bd03ca9880ffe78bc9a74285c
mathsyouth <lh.zeng@hotmail.com>
2022-09-13 16:13:28 +0800

    add glfw demo

--- another_cmake_tutorial/main.cpp
+++ another_cmake_tutorial/main.cpp
@@ -1,10 +1,36 @@
 #include <iostream>
 #include <adder.h>
+#include <GLFW/glfw3.h>
 
 
 int main()
 {
   std::cout << "Hello World!" << std::endl;
   std::cout << add(3.14f, 4.93f) << std::endl;
+
+  GLFWwindow *window;
+  if( !glfwInit() )
+  {
+      fprintf( stderr, "Failed to initialize GLFW\n" );
+      exit( EXIT_FAILURE );
+  }
+  window = glfwCreateWindow( 300, 300, "Gears", NULL, NULL );
+  if (!window)
+  {
+      fprintf( stderr, "Failed to open GLFW window\n" );
+      glfwTerminate();
+      exit( EXIT_FAILURE );
+  }
+     // Main loop
+  while( !glfwWindowShouldClose(window) )
+  {
+
+      // Swap buffers
+      glfwSwapBuffers(window);
+      glfwPollEvents();
+  }
+
+  // Terminate GLFW
+  glfwTerminate();
   return 1;
  }
